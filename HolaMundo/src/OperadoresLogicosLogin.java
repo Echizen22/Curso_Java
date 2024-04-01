573fa2aecb939010b7e0ea8e7edf00b59481dd4b
Echizen22 <erixon.arce.justiniano@gmail.com>
2024-04-01 11:41:01 +0200

    Ejemplo login usando arreglos

--- HolaMundo/src/OperadoresLogicosLogin.java
+++ HolaMundo/src/OperadoresLogicosLogin.java
@@ -3,11 +3,19 @@ import java.util.Scanner;
 public class OperadoresLogicosLogin {
     public static void main(String[] args) {
 
-        String username = "andres";
-        String password = "12345";
+        /*String[] usernames = new String[3];
+        String[] passwords = new String[3];
+        usernames[0] = "andres";
+        passwords[0] = "12345";
 
-        String username2 = "admin";
-        String password2 = "12345";
+        usernames[1] = "admin";
+        passwords[1] = "12345";
+
+        usernames[2] = "pepe";
+        passwords[2] = "12345";*/
+
+        String[] usernames = {"andres", "admin", "pepe"};
+        String[] passwords = {"123", "1234", "12345"};
 
         Scanner scanner = new Scanner(System.in);
 
@@ -19,16 +27,18 @@ public class OperadoresLogicosLogin {
 
         boolean esAutenticado = false;
 
-        if( (username.equals(u) && password.equals(p)) ||
-                (username2.equals(u) && password2.equals(p)) ) {
-            esAutenticado = true;
-        } else {
-            System.out.println("Username o contraseña incorrecto!");
+        for(int i = 0; i < usernames.length; i++) {
+            if( (usernames[i].equals(u) && passwords[i].equals(p)) ) {
+                esAutenticado = true;
+                break;
+            }
         }
+
 
         if(esAutenticado) {
             System.out.println("Bienvenido usuario ".concat(u).concat("!"));
         } else {
+            System.out.println("Username o contraseña incorrecto!");
             System.out.println("Lo siento, requiere autenticación");
         }
     }
