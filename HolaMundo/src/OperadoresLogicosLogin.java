ea50bcbf221c277e3e9fc8069281e8f47daf8a22
Echizen22 <erixon.arce.justiniano@gmail.com>
2024-04-01 15:31:10 +0200

    Ejemplo login con operador ternario

--- HolaMundo/src/OperadoresLogicosLogin.java
+++ HolaMundo/src/OperadoresLogicosLogin.java
@@ -28,18 +28,23 @@ public class OperadoresLogicosLogin {
         boolean esAutenticado = false;
 
         for(int i = 0; i < usernames.length; i++) {
-            if( (usernames[i].equals(u) && passwords[i].equals(p)) ) {
+            esAutenticado = (usernames[i].equals(u) && passwords[i].equals(p)) ? true: esAutenticado;
+        
+            /*if( (usernames[i].equals(u) && passwords[i].equals(p)) ) {
                 esAutenticado = true;
                 break;
-            }
+            }*/
         }
 
 
-        if(esAutenticado) {
+        String mensaje = esAutenticado ? "Bienvenido usuario ".concat(u).concat("!") :
+                "Username o contraseña incorrecto!\nLo sentimos, requiere autenticación";
+        System.out.println("mensaje = " + mensaje);
+        /*if(esAutenticado) {
             System.out.println("Bienvenido usuario ".concat(u).concat("!"));
         } else {
             System.out.println("Username o contraseña incorrecto!");
             System.out.println("Lo siento, requiere autenticación");
-        }
+        }*/
     }
 }
