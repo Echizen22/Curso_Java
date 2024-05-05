61acc30f6809eeeb99b3ab6ac0d1c06f937aac5a
Echizen22 <erixon.arce.justiniano@gmail.com>
2024-05-06 00:50:08 +0200

    Línea de comando ejemplo calculadora

--- HolaMundo/src/ArgumentosLineaComando.java
+++ HolaMundo/src/ArgumentosLineaComando.java
@@ -2,12 +2,12 @@ public class ArgumentosLineaComando {
     public static void main(String[] args) {
 
         if( args.length == 0){
-            System.out.println("Debe ingresar argumentos!");
+            System.out.println("Debe ingresar argumentos o parámetros!");
             System.exit(-1);
         }
 
         for(int i = 0; i < args.length; i++){
-            System.out.println("Argumentos nro " + i + ": " + args[i]);
+            System.out.println("Argumentos nª " + i + ": " + args[i]);
         }
     }
 }
